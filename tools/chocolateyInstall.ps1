cdf0284424d88010b76e62c41b47f9791412e64c
Aaron Sherber <aaron@sherber.com>
2017-08-01 11:58:34 -0400

    v3.38

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@
    packageName = 'highlight'
    fileType = 'exe'
    silentArgs = '/verysilent'
-   url64bit = 'http://www.andre-simon.de/zip/highlight-setup-3.37-x64.exe'
-   checksum64 = 'fc78b8e804c52c6c8945758e64c8bf87afc930480f319921285c89a66237dcb7'
+   url64bit = 'http://www.andre-simon.de/zip/highlight-setup-3.38-x64.exe'
+   checksum64 = 'df5300fef1f867cf066282be8b317da4314b38c08858c5aa6cd1b513613f96fe'
    checksumType64 = 'sha256'
-   url = 'http://www.andre-simon.de/zip/highlight-setup-3.37.exe'
-   checksum = '00caa146cf530b667e504762382fdb4bef9e83df10db067c132718f2989c5636'
+   url = 'http://www.andre-simon.de/zip/highlight-setup-3.38.exe'
+   checksum = '0a3e813b69770cc04c7336ba6c36d5cf34327dd1286547d6f268ef089edf88ae'
    checksumType = 'sha256'
 }
 
